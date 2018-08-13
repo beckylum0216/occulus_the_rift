ad464270faa697e8b7a89aad03dcacd792d45e7e
Rebecca <beckylum0214@gmail.com>
2018-08-13 11:43:32 +0800

    updated AABB collissions

--- AABB.cpp
+++ AABB.cpp
@@ -1,5 +1,6 @@
 
 #include "AABB.h"
+#include "gl/glut.h"
 
 AABB::AABB()
 {
@@ -10,4 +11,37 @@ AABB::~AABB()
 {
 
 }
+
+void AABB::SetMinPoint(Point inputMinPoint)
+{
+	minPoint = inputMinPoint;
+}
+
+Point AABB::GetMinPoint()
+{
+	return minPoint;
+}
+
+void AABB::SetMaxPoint(Point inputMaxPoint)
+{
+	maxPoint = inputMaxPoint;
+}
+
+
+Point AABB::GetMaxPoint()
+{
+	return maxPoint;
+}
+
+
+bool AABB::AABBtoAABB(const Box &objectOne, const Box &objectTwo)
+{
+	return (objectOne.maxBox.x > objectTwo.minBox.x &&
+			objectOne.minBox.x < objectTwo.maxBox.x &&
+			objectOne.maxBox.y > objectTwo.minBox.y &&
+			objectOne.minBox.y < objectTwo.maxBox.y &&
+			objectOne.maxBox.z > objectTwo.minBox.z &&
+			objectOne.minBox.z < objectTwo.maxBox.z);
+}
+
 
--- AABB.h
+++ AABB.h
@@ -1,14 +1,30 @@
 #pragma once
 
+#include "gl/glut.h"
+
+struct Point {
+	GLdouble x, y, z;
+};
+
+struct Box {
+	Point minBox;
+	Point maxBox;
+};
 
 class AABB
 {
 	public:
 		AABB();
 		virtual~AABB();
+		void SetMinPoint(Point inputMin);
+		Point GetMinPoint();
+		void SetMaxPoint(Point inputMax);
+		Point GetMaxPoint();
+		bool AABBtoAABB(const Box &objectOne, const Box &objectTwo);
 
 	private:
-
+		Point minPoint;
+		Point maxPoint;
 
 
 };
