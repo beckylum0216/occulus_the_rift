7f1ad571c97411ce8893a7aeec3af80faa33fbb8
Brandon <brandondean144@gmail.com>
2018-08-15 22:02:06 +0800

    input manager works for normal keys. Will add special keys later.

--- Game.cpp
+++ Game.cpp
@@ -65,73 +65,143 @@ void Game::ShutDown() {
 	exit(0);
 }
 
-void Game::inputDown(unsigned char key, int x, int y) {
-	switch (key) {
-	case 'a':
-	case 'A':
-		user.DirectionLR(-1);
-		break;
-	case 'd':
-	case 'D':
-		user.DirectionLR(1);
-		break;
-	case 'w':
-	case 'W':
-		user.DirectionFB(1);
-		break;
-	case 's':
-	case 'S':
-		user.DirectionFB(-1);
-		break;
-	case 'q':
-	case 'Q':
-		user.DirectionLookLR(-1);
-		break;
-	case 'e':
-	case 'E':
-		user.DirectionLookLR(1);
-		break;
+void Game::inputDown(unsigned char key, int x, int y) 
+{
+	KeyCode button = KeyToButton(key);
+	SetButtonDown(true, button);
+	return;
+}
+
+void Game::inputUp(unsigned char key, int x, int y) 
+{
+	KeyCode button = KeyToButton(key);
+	SetButtonUp(true, button);
+	return;
+}
+
+void Game::SetButtonDown(bool value, KeyCode button) 
+{
+	KeyDown[button] = value;
+	if (value)
+	{
+		switch (button)
+		{
+		case keyW:
+			user.DirectionFB(1);
+			break;
+		case keyA:
+			user.DirectionLR(-1);
+			break;
+		case keyS:
+			user.DirectionFB(-1);
+			break;
+		case keyD:
+			user.DirectionLR(1);
+			break;
+		case keyQ:
+			user.DirectionLookLR(-1);
+			break;
+		case keyE:
+			user.DirectionLookLR(1);
+			break;
+		case keyR:
+			user.DirectionLookUD(1);
+			break;
+		case keyF:
+			user.DirectionLookUD(-1);
+			break;
+		}
+	}
+	return;
+}
+
+void Game::SetButtonUp(bool value, KeyCode button) 
+{
+	KeyUp[button] = value;
+	if (value)
+	{
+		SetButtonDown(false, button);
+
+		switch (button)
+		{
+		case keyW:
+		case keyS:
+			user.DirectionFB(0);
+			break;
+		case keyA:
+		case keyD:
+			user.DirectionLR(0);
+			break;
+		case keyQ:
+		case keyE:
+			user.DirectionLookLR(0);
+			break;
+		case keyR:
+		case keyF:
+			user.DirectionLookUD(0);
+			break;
+		}
+	}
+	return;
+}
+
+bool Game::GetButtonDown(KeyCode button) 
+{
+	return KeyDown[button];
+}
+
+bool Game::GetButtonUp(KeyCode button) 
+{
+	return KeyUp[button];
+}
+
+KeyCode Game::KeyToButton(char key)
+{
+	KeyCode button;
+	switch (key)
+	{
 	case 27:
 		ShutDown();
 		break;
-	}
-}
-
-void Game::inputUp(unsigned char key, int x, int y) {
-	switch (key) {
-	case 'a':
-	case 'A':
-	case 'd':
-	case 'D':
-		user.DirectionLR(0);
-		break;
 	case 'w':
 	case 'W':
+		button = keyW;
+		break;
+	case 'a':
+	case 'A':
+		button = keyA;
+		break;
 	case 's':
 	case 'S':
-		user.DirectionFB(0);
+		button = keyS;
+		break;
+	case 'd':
+	case 'D':
+		button = keyD;
+		break;
+	case 'r':
+	case 'R':
+	case '8':
+		button = keyR;
 		break;
 	case 'q':
 	case 'Q':
+	case '4':
+		button = keyQ;
+		break;
+	case 'f':
+	case 'F':
+	case '2':
+		button = keyF;
+		break;
 	case 'e':
 	case 'E':
-		user.DirectionLookLR(0);
+	case '6':
+		button = keyE;
+		break;
+	case ' ':
+		button = keySpace;
 		break;
 	}
-}
-
-void Game::SetKeyDown(bool value, KeyCode key) {
-
-}
-
-void Game::SetKeyUp(bool value, KeyCode key) {
-
-}
-
-bool Game::GetKeyDown(KeyCode key) {
-	return KeyDown[key];
-}
-
-bool Game::GetKeyUp(KeyCode key) {
-	return KeyUp[key];
+	return button;
 }
